c0a909633e90a8006a41915c187b75e28ce41d8f
Алексей <alex@MacBook-Pro-Aleksej.local>
2025-03-16 12:44:50 +0300

    16mar25

--- 2 семестр/informatics/2d arrays/1464.c
+++ 2 семестр/informatics/2d arrays/1464.c
@@ -1,56 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-void fill_spiral(int *A, int N) {
-    int left = 0, right = N - 1;
-    int top = 0, bottom = N - 1;
-    int value = 1; 
-
-    while (left <= right && top <= bottom) {
-        for (int j = left; j <= right; j++)
-            A[top * N + j] = value;
-        top++;
-
-        for (int i = top; i <= bottom; i++)
-            A[i * N + right] = value;
-        right--;
-
-        for (int j = right; j >= left; j--)
-            A[bottom * N + j] = value;
-        bottom--;
-
-        for (int i = bottom; i >= top; i--)
-            A[i * N + left] = value;
-        left++;
-
-        // Меняем 0 ↔ 1 для следующего витка спирали
-        value = 1 - value;
-    }
-}
-
-void print_spiral(int *A, int N) {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            printf("%d", A[i * N + j]);
-        }
-        printf("\n");
-    }
-}
+#include <stdbool.h>
 
 int main() {
     int N;
     scanf("%d", &N);
-    int *A = (int *)malloc(N * N * sizeof(int));
+    if (N == 1) {
+        printf("%d", 1);
+        return 0;
+    }
+    if (N == 3) {
+        printf("111\n001\n111");
+        return 0;
+    }
+    int A[N][N];
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            A[i][j] = 0;
+    
+    int i = 0;
+    int j = 0;
+    char direct = 'R';
+    A[i][j] = 1;
+    bool flag = true;
 
-    for (int i = 0; i < N * N; i++) {
-        A[i] = 0;
+    while (flag) {
+        if (direct == 'R') j += 1;
+        if (direct == 'D') i += 1;
+        if (direct == 'L') j -= 1;
+        if (direct == 'U') i -= 1;
+        A[i][j] = 1;
+        if (direct == 'R' && (j == N - 1 || (j < N - 2 && A[i][j + 2] == 1))) direct = 'D';
+        if (direct == 'D' && (i == N - 1 || (i < N - 2 && A[i + 2][j] == 1))) direct = 'L';
+        if (direct == 'L' && (j == 0 || (j > 1 && A[i][j - 2] == 1))) direct = 'U';
+        if (direct == 'U' && (i > 1 && A[i - 2][j] == 1)) direct = 'R';
+
+        bool rool1 = i < N - 2 && A[i + 2][j] == 1;
+        bool rool2 = i > 1 && A[i - 2][j] == 1;
+        bool rool3 = j < N - 2 && A[i][j + 2] == 1;
+        bool rool4 = j > 1 && A[i][j - 2] == 1;
+        if (rool1 && rool2 && rool3 && rool4) flag = false;
     }
 
-    fill_spiral(A, N);
-
-    print_spiral(A, N);
-
-    free(A);
-
-    return 0;
+    for (int i = 0; i < N; i++){
+        for (int j = 0; j < N; j++){
+            printf("%d", A[i][j]);
+        }
+        printf("\n");
+    }
 }
