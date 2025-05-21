17b2621cbecb2c8acedb209e4e550fe102cdb263
Алексей <alex@MacBook-Pro-Aleksej.local>
2025-05-21 11:56:57 +0300

    21may25

---
+++ 2 семестр/автотест/task10/10.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+double weight(double *a, int n, int m, int j)
+{
+    double sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += a[i * m + j] * a[i * m + j];
+    return sum;
+}
+
+void process(double *a, int n, int m)
+{
+    int i, k, j;
+    double w;
+    for (k = 0; k < m - 1; k++)
+    {
+        for(j = 0; j < m - 1;j++)
+        {
+            if(weight(a,n,m,j) > weight(a,n,m,j + 1))
+            for(i = 0; i < n; i++)
+            {
+                // операция swap
+                w = a[i * m +j];
+                a[i * m + j] = a[i * m + (j + 1)];
+                a[i * m + (j + 1)] = w;
+            }
+        }
+    }
+}
+
+void multipl(double *a, double *c, int n, int m)
+{
+    int i, j;
+    double g = 0;
+    for (i = 0; i < n; i++)
+    {
+        g = 0;
+        for(j = 0; j < m; j++)
+            g += a[i * m + j] * a[(n - 1) * m + j];
+        c[i] = g;
+    }
+}
+
+int main (void)
+{
+    int m, n, i, j;
+    double *a, *c;
+    FILE *in = fopen("input.txt","r");
+    FILE *out = fopen("output.txt","w");
+    if (in == NULL)
+        return -1;
+    if (out == NULL)
+    {
+        fclose(in);
+        return -1;
+    }
+    if (fscanf(in, "%d", &n) != 1)
+    {
+        fclose(in);
+        fclose(out);
+        return -1;
+    }
+    if (fscanf(in, "%d", &m) != 1)
+    {
+        fclose(in);fclose(out);
+        return -1;
+    }
+    if (n < 0 || m < 0)
+    {
+        fclose(in);
+        fclose(out);
+        return -1;
+    }
+    if (!(a = (double*)malloc(n * m * sizeof(double))))
+    {
+        fclose(in);
+        fclose(out);
+        return -1;
+    }
+    if (!(c = (double*)malloc(n * sizeof(double))))
+    {
+        free(a);
+        fclose(in);
+        fclose(out);
+        return -1;
+    }
+    for (i = 0; i < n; i++)
+        c[i]=0;
+    for(i = 0; i < n * m; i++)
+        if (fscanf(in,"%lf ", &a[i]) != 1)
+        {
+            free(a);
+            free(c);
+            fclose(in);
+            fclose(out);
+            return -1;
+        }
+    fclose(in);
+    process(a,n,m);
+    multipl(a,c,n,m);
+    fprintf(out, "%d %d\n", n, m);
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0;j < m; j++)
+            fprintf(out,"%lf  ", a[i * m + j]);
+        fprintf(out,"\n");
+    }
+    fprintf(out,"\n");
+    for(i = 0; i < n; i++)
+        fprintf(out, "%lf\n", c[i]);
+    free(a);
+    free(c);
+    fclose(out);
+    return 0;
+}
---
+++ 2 семестр/автотест/task11/11.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define BOUND 10000
+
+double weight(double *a,int m,int i)
+{
+    double max = -BOUND;
+    double min = BOUND;
+
+    for (int j = 0; j < m ;j++){
+        if (a[i * m + j] > max)
+            max = a[i * m + j];
+        if (a[i * m + j] < min)
+            min = a[i * m + j];
+    }
+    return max - min;
+}
+
+void process(double*a,int n,int m)
+{
+    int i,k,j;
+    double w;
+    for(k = 0; k < n - 1; k++)
+    {
+        for(i = 0;i < n - 1;i++)
+        {
+            if(weight(a, m, i) < weight(a, m, i + 1))
+            for (j = 0; j < m; j++)
+            {
+                w=a[i*m+j];
+                a[i*m+j]=a[(i+1)*m+j];
+                a[(i+1)*m+j]=w;
+            }
+        }
+    }
+}
+
+
+void multipl(double *a, double *c, int n, int m)
+{
+    int i, j;
+    double g = 0;
+    for (i = 0; i < n; i++)
+    {
+        g = 0;
+        for(j = 0; j < m; j++)
+            g += a[i * m + j] * a[(n - 1) * m + j];
+        c[i] = g;
+    }
+}
+
+int main (void)
+{
+    int m, n, i, j;
+    double *a, *c;
+    FILE *in = fopen("input.txt","r");
+    FILE *out = fopen("output.txt","w");
+    if (in == NULL)
+        return -1;
+    if (out == NULL)
+    {
+        fclose(in);
+        return -1;
+    }
+    if (fscanf(in, "%d", &n) != 1)
+    {
+        fclose(in);
+        fclose(out);
+        return -1;
+    }
+    if (fscanf(in, "%d", &m) != 1)
+    {
+        fclose(in);fclose(out);
+        return -1;
+    }
+    if (n < 0 || m < 0)
+    {
+        fclose(in);
+        fclose(out);
+        return -1;
+    }
+    if (!(a = (double*)malloc(n * m * sizeof(double))))
+    {
+        fclose(in);
+        fclose(out);
+        return -1;
+    }
+    if (!(c = (double*)malloc(n * sizeof(double))))
+    {
+        free(a);
+        fclose(in);
+        fclose(out);
+        return -1;
+    }
+    for (i = 0; i < n; i++)
+        c[i]=0;
+    for(i = 0; i < n * m; i++)
+        if (fscanf(in,"%lf ", &a[i]) != 1)
+        {
+            free(a);
+            free(c);
+            fclose(in);
+            fclose(out);
+            return -1;
+        }
+    fclose(in);
+    process(a,n,m);
+    multipl(a,c,n,m);
+    fprintf(out, "%d %d\n", n, m);
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0;j < m; j++)
+            fprintf(out,"%lf  ", a[i * m + j]);
+        fprintf(out,"\n");
+    }
+    fprintf(out,"\n");
+    for(i = 0; i < n; i++)
+        fprintf(out, "%lf\n", c[i]);
+    free(a);
+    free(c);
+    fclose(out);
+    return 0;
+}
