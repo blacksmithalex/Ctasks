a7ab643470d08fad4c6d66a2e0e213503bbdfae5
Алексей <alex@MacBook-Pro-Aleksej.local>
2025-05-28 17:42:27 +0300

    28may25

--- 2 семестр/новый автотест/task109/109.c
+++ 2 семестр/новый автотест/task109/109.c
@@ -64,17 +64,24 @@ int main(void) {
         }
     }
 
+    int *Acopy = (int *)malloc(L * K * sizeof(int));
+    for (int i = 0; i < L; i++) {
+        for (int j = 0; j < K; j++) {
+            Acopy[i * K + j] = A[i * K + j];
+        }
+    }
+
     // Модификация элементов
     for (int i = 0; i < L; i++) {
         for (int j = 0; j < K; j++) {
             if (rows_with_segment[i] == 1 && cols_with_segment[j] == 1) {
-                int target_value = A[i * K + j];
+                int target_value = Acopy[i * K + j];
                 bool is_in_all_rows = true;
 
                 for (int curi = 0; curi < L; curi++) {
                     bool found_in_row = false;
                     for (int curj = 0; curj < K; curj++) {
-                        if (A[curi * K + curj] == target_value) {
+                        if (Acopy[curi * K + curj] == target_value) {
                             found_in_row = true;
                             break;
                         }
